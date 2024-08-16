9d48549def9378a8656e079b11c6999f07372af0
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2024-08-16 10:57:18 +0530

    Added Button.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Fugaz_One, Inter } from "next/font/google";
+import { Fugaz_One, Open_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const opensans = Open_Sans({ subsets: ["latin"] });
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata = {
@@ -12,17 +12,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   const header = (
     <header className='p-4 sm:p-8 flex items-center justify-between gap-4 '>
-      <h1 className={"text-base sm:text-lg " + fugaz.className}>MoodTracker</h1>
+      <h1 className={"text-base sm:text-lg textGradient " + fugaz.className}>
+        MoodTracker
+      </h1>
     </header>
   );
-  const footer = <footer>Footer</footer>;
+  const footer = (
+    <footer className='p-4 sm:p-8 grid place-items-center'>
+      <p className={"text-indigo-500" + fugaz.className}>
+        <span className='font-semibold'>&copy;{new Date().getFullYear()}</span>{" "}
+        Pranav Pise
+      </p>
+    </footer>
+  );
 
   return (
     <html lang='en'>
       <body
         className={
-          "w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col " +
-          inter.className
+          "w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800 " +
+          opensans.className
         }>
         {header}
         {children}
