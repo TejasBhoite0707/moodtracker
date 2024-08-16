74cc22a1b545e8dd7c328a9f7c07e0e7a776c853
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2024-08-17 00:55:12 +0530

    Project Complete (Almost)

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,14 +1,14 @@
 import Dashboard from "@/components/Dashboard";
-import Login from "@/components/Login";
 import Main from "@/components/Main";
 
 export const metadata = {
-  title: "MoodTracker · Dashboard",
+  title: "Broodl · Dashboard",
 };
 
 export default function DashboardPage() {
-  const isAuthenticated = false;
-  const children = isAuthenticated ? <Dashboard /> : <Login />;
-
-  return <Main>{children}</Main>;
+  return (
+    <Main>
+      <Dashboard />
+    </Main>
+  );
 }
--- components/Hero.js
+++ components/Hero.js
@@ -1,11 +1,14 @@
 import { Fugaz_One } from "next/font/google";
 import React from "react";
 import Button from "./Button";
+import Calendar from "./Calendar";
+import Link from "next/link";
+import CallToAction from "./CallToAction";
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
 
 export default function Hero() {
   return (
-    <div className='py-4 md:py-10 flex flex-col gap-4 sm:gap-8'>
+    <div className='py-4 md:py-10 flex flex-col gap-8 sm:gap-10'>
       <h1
         className={
           "text-5xl sm:text-6xl md:text-7xl text-center " + fugaz.className
@@ -17,10 +20,8 @@ export default function Hero() {
         Create your mood record and see how you feel on{" "}
         <span className='font-semibold'>every day of every year.</span>
       </p>
-      <div className='grid grid-cols-2 gap-4 w-fit mx-auto'>
-        <Button text='Sign Up' />
-        <Button text='Login' dark />
-      </div>
+      <CallToAction />
+      <Calendar demo />
     </div>
   );
 }
