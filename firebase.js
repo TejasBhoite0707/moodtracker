651e5f18cb4be1162b94f86da88ba8287db0464e
surajpatil630304@gmail.com <tejasbhoite3006@gmail.com>
2024-10-03 13:50:16 +0530

    Update1

--- firebase.js
+++ firebase.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
 };
-
+//The Captain
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
